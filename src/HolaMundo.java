f958f8d04cc545a477635dc8e2af9eb6001d256d
Carlos Antonio Sanchez <unyty02@gmail.com>
2021-06-09 21:49:58 -0500

    Leccion actual: Tarea 2/ leccion 13 clase scanner

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner; //importar
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,13 +13,14 @@
  */
 public class HolaMundo {
     public static void main(String[] args) {
-        var nombre = "poncho";
         
-        System.out.println("nueva linea: \n"+nombre);
-        System.out.println("tabulador: \t"+nombre);
-        System.out.println("retroceso: \b"+nombre);
-        System.out.println("comilla simple: \'"+nombre+"\'");
-        System.out.println("comilla doble: \""+nombre+"\"");
+        System.out.println("Proporciona el titulo: ");
+        Scanner consola = new Scanner(System.in);
+        var titulo = consola.nextLine();
+        //System.out.println("usuario = " + usuario);
+        System.out.println("Proporciona el autor");
         
+        var autor = consola.nextLine();
+        System.out.println(titulo + " fue escrito por "+autor);
     }
 }
