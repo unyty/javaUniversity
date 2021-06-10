28844a4cd6728fd8a0bf3ac79fde2dfc62619148
Carlos Antonio Sanchez <unyty02@gmail.com>
2021-06-09 22:02:01 -0500

    Base hola mundo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -10,33 +10,6 @@
  */
 public class HolaMundo {
     public static void main(String[] args) {
-        //definir variable
-        int miVariableEntera=10;
-        System.out.println(miVariableEntera);
-        //modificar variable
-        miVariableEntera=5;
-        System.out.println(miVariableEntera);
         
-        String miVariableCadena="variable string";
-        
-        System.out.println(miVariableCadena);
-        
-        miVariableCadena="chao";
-        System.out.println(miVariableCadena);
-        
-        //inferencia de variable
-        var miVariableEntera2 =10;
-        System.out.println(miVariableEntera2);
-        
-        var miVariableCadena2 = "raro inferencia ...";
-        System.out.println(miVariableCadena2);
-        
-        System.out.println("miVariableCadena2 = " + miVariableCadena2);
-        
-        //declaraciones raras de variables
-        var miVariable =1;
-        var _miVariable=2;
-        var $miVariable=3; //WTF
-        //var miVariáble=4; //jamas hacer esto pero se puede
     }
 }
